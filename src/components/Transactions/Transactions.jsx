f13603d1cbf7a394b8cfdbefb5ef0a2864174a75
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-01 14:01:43 +0100

    Add Screenshot

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -52,7 +52,7 @@ export default function Transactions({ setDarkMode, darkMode }) {
     <section className={`transactions ${darkMode ? 'dark' : ''}`}>
       <svg
         onClick={handleDarkmode}
-        className={`transactions-icon`}
+        className={`transactions-icon ${darkMode ? 'dark' : ''}`}
         xmlns='http://www.w3.org/2000/svg'
         viewBox='0 0 512 512'>
         <path d='M448 256c0-106-86-192-192-192V448c106 0 192-86 192-192zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z' />
