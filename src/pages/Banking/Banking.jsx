080971632f28be29701376b283b9ebb2007a8df1
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-01 13:42:29 +0100

    Add Darkmode

--- src/pages/Banking/Banking.jsx
+++ src/pages/Banking/Banking.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import Transactions from '../../components/Transactions/Transactions'
 
 export default function Banking() {
+  const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <div className='wrapper'>
-      <h1>Banana Bank</h1>
-      <Transactions />
+    <div className={`wrapper ${darkMode ? 'dark' : ''}`}>
+      <h1 className={darkMode ? 'dark' : ''}>Banana Bank</h1>
+      <Transactions
+        setDarkMode={setDarkMode}
+        darkMode={darkMode}
+      />
     </div>
   )
 }
